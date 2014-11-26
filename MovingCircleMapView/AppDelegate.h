52e0931df60bb47900ae2590e365de50a1d088c1
Carlo Luis Bation <cbation@isbx.com>
2014-11-26 09:35:30 +0800

    Initial Commit

---
+++ MovingCircleMapView/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  MovingCircleMapView
+//
+//  Created by Carlo Bation on 11/26/14.
+//  Copyright (c) 2014 CL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ MovingCircleMapView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MovingCircleMapView
+//
+//  Created by Carlo Bation on 11/26/14.
+//  Copyright (c) 2014 CL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
